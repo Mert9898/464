0bfade95fc23290cbe4b8977db5f7d925d60e2ca
Mert98 <smilinggreenfrog31@gmail.com>
2024-05-30 20:51:23 +0300

    Final

--- 464.py
+++ 464.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D, Dropout, BatchNormalization, GRU
+from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D, BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.regularizers import l2
@@ -42,76 +42,142 @@ def load_and_sample_dataset(dataset_name, split, sample_size):
 
 sample_size = 500
 
-dataset_1 = load_and_sample_dataset(
-    "hkust-nlp/deita-quality-scorer-data", 'validation', sample_size)
-dataset_2 = load_and_sample_dataset(
-    "turkish-nlp-suite/vitamins-supplements-reviews", 'train', sample_size)
-dataset_3 = load_and_sample_dataset(
-    "turkish-nlp-suite/beyazperde-top-300-movie-reviews", 'train', sample_size)
+datasets = {
+    "Dataset 1": load_and_sample_dataset("hkust-nlp/deita-quality-scorer-data", 'validation', sample_size),
+    "Dataset 2": load_and_sample_dataset("turkish-nlp-suite/vitamins-supplements-reviews", 'train', sample_size),
+    "Dataset 3": load_and_sample_dataset("turkish-nlp-suite/beyazperde-top-300-movie-reviews", 'train', sample_size)
+}
 
-processed_data_1 = [preprocess_text(entry['input']) for entry in dataset_1]
-processed_data_2 = [preprocess_text(
-    entry['product_name'], language='turkish') for entry in dataset_2]
-processed_data_3 = [preprocess_text(
-    entry['movie'], language='turkish') for entry in dataset_3]
+all_texts = []
+all_labels = []
 
-texts = processed_data_1 + processed_data_2 + processed_data_3
-labels_1 = np.array([i % 2 for i in range(len(processed_data_1))])
-labels_2 = np.array([i % 2 for i in range(len(processed_data_2))])
-labels_3 = np.array([i % 2 for i in range(len(processed_data_3))])
-labels = np.concatenate([labels_1, labels_2, labels_3])
+histories = {}
+
+for dataset_name, dataset in datasets.items():
+
+    print(f"{dataset_name} first entry:", dataset[0])
+
+    if dataset_name == "Dataset 1":
+        processed_data = [preprocess_text(entry['input']) for entry in dataset]
+    elif dataset_name == "Dataset 2":
+        processed_data = [preprocess_text(
+            entry['text'], language='turkish') for entry in dataset]
+    elif dataset_name == "Dataset 3":
+        processed_data = [preprocess_text(
+            entry['text'], language='turkish') for entry in dataset]
+
+    labels = np.array([i % 2 for i in range(len(processed_data))])
+
+    all_texts.extend(processed_data)
+    all_labels.extend(labels)
+
+    label_encoder = LabelEncoder()
+    labels = label_encoder.fit_transform(labels)
+
+    tokenizer = Tokenizer(num_words=5000, lower=True, oov_token='UNK')
+    tokenizer.fit_on_texts(processed_data)
+    word_index = tokenizer.word_index
+    vocab_size = len(word_index) + 1
+
+    sequences = tokenizer.texts_to_sequences(processed_data)
+    padded_sequences = pad_sequences(sequences, maxlen=100)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        padded_sequences, labels, test_size=0.2, random_state=42)
+
+    model = Sequential()
+    model.add(Embedding(vocab_size, 100, input_length=100))
+    model.add(SpatialDropout1D(0.6))
+    model.add(LSTM(64, return_sequences=True,
+              dropout=0.6, recurrent_dropout=0.6))
+    model.add(LSTM(32, dropout=0.6, recurrent_dropout=0.6))
+    model.add(BatchNormalization())
+    model.add(Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01)))
+
+    model.compile(loss='binary_crossentropy', optimizer=RMSprop(
+        learning_rate=1e-4), metrics=['accuracy'])
+
+    epochs = 50
+    batch_size = 32
+
+    early_stopping = EarlyStopping(
+        monitor='val_loss', patience=10, min_delta=0.0001, restore_best_weights=True)
+    model_checkpoint = ModelCheckpoint(
+        'best_model_' + dataset_name + '.keras', save_best_only=True, monitor='val_loss', mode='min')
+    reduce_lr = ReduceLROnPlateau(
+        monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
+
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
+                        validation_split=0.3, callbacks=[early_stopping, model_checkpoint, reduce_lr])
+
+    histories[dataset_name] = history
+
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=2)
+    print(f'{dataset_name} - Loss: {loss}, Accuracy: {accuracy}')
+
+
+def plot_training_history(histories):
+    for dataset_name, history in histories.items():
+        plt.figure(figsize=(12, 4))
+        plt.subplot(1, 2, 1)
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.title(f'Loss - {dataset_name}')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        plt.subplot(1, 2, 2)
+        plt.plot(history.history['accuracy'], label='Training Accuracy')
+        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.title(f'Accuracy - {dataset_name}')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+
+        plt.show()
+
+
+plot_training_history(histories)
 
 label_encoder = LabelEncoder()
-labels = label_encoder.fit_transform(labels)
+all_labels = label_encoder.fit_transform(all_labels)
 
 tokenizer = Tokenizer(num_words=5000, lower=True, oov_token='UNK')
-tokenizer.fit_on_texts(texts)
+tokenizer.fit_on_texts(all_texts)
 word_index = tokenizer.word_index
 vocab_size = len(word_index) + 1
 
-sequences = tokenizer.texts_to_sequences(texts)
+sequences = tokenizer.texts_to_sequences(all_texts)
 padded_sequences = pad_sequences(sequences, maxlen=100)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    padded_sequences, labels, test_size=0.2, random_state=42)
+    padded_sequences, all_labels, test_size=0.2, random_state=42)
 
 model = Sequential()
 model.add(Embedding(vocab_size, 100, input_length=100))
 model.add(SpatialDropout1D(0.6))
-model.add(LSTM(64, return_sequences=True, dropout=0.6,
-          recurrent_dropout=0.6))
-
+model.add(LSTM(64, return_sequences=True, dropout=0.6, recurrent_dropout=0.6))
 model.add(LSTM(32, dropout=0.6, recurrent_dropout=0.6))
 model.add(BatchNormalization())
-
 model.add(Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01)))
 
 model.compile(loss='binary_crossentropy', optimizer=RMSprop(
     learning_rate=1e-4), metrics=['accuracy'])
 
-epochs = 50
-batch_size = 32
-
-early_stopping = EarlyStopping(monitor='val_loss', patience=10,
-                               min_delta=0.0001, restore_best_weights=True)
-model_checkpoint = ModelCheckpoint(
-    'best_model.keras', save_best_only=True, monitor='val_loss', mode='min')
-reduce_lr = ReduceLROnPlateau(
-    monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
-
 history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
                     validation_split=0.3, callbacks=[early_stopping, model_checkpoint, reduce_lr])
 
 loss, accuracy = model.evaluate(X_test, y_test, verbose=2)
-print(f'Loss: {loss}, Accuracy: {accuracy}')
+print(f'General - Loss: {loss}, Accuracy: {accuracy}')
 
 
-def plot_training_history(history):
+def plot_general_training_history(history):
     plt.figure(figsize=(12, 4))
     plt.subplot(1, 2, 1)
     plt.plot(history.history['loss'], label='Training Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
-    plt.title('Loss')
+    plt.title('Loss - General')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
@@ -119,7 +185,7 @@ def plot_training_history(history):
     plt.subplot(1, 2, 2)
     plt.plot(history.history['accuracy'], label='Training Accuracy')
     plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-    plt.title('Accuracy')
+    plt.title('Accuracy - General')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
@@ -127,12 +193,4 @@ def plot_training_history(history):
     plt.show()
 
 
-plot_training_history(history)
-
-example_entry_1 = dataset_1[0]
-example_entry_2 = dataset_2[0]
-example_entry_3 = dataset_3[0]
-
-print("Example Entry from Dataset 1:", example_entry_1)
-print("Example Entry from Dataset 2:", example_entry_2)
-print("Example Entry from Dataset 3:", example_entry_3)
+plot_general_training_history(history)
